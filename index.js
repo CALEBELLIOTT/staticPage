03eb538bb0d9066d2a4c32681023d34ccbaa4227
Caleb Elliott <caleb123elliott@gmail.com>
2022-09-02 11:19:50 -0600

    eh may be broken idk

--- index.js
+++ index.js
@@ -1,19 +1,15 @@
-import React from "react"
-import ReactDOM from "react-dom"
-
-
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 
 function MainContent() {
   return (
     <div>
-
       <h1>Welcome to the main content of the page</h1>
       <p>I hope you enjoy!</p>
     </div>
   )
 }
 
-ReactDOM.render(
-  <MainContent />,
-  document.getElementById('root')
-)
+ReactDOM.createRoot(document.getElementById('root')).render(<MainContent></MainContent>)
+
+console.log('hello');
